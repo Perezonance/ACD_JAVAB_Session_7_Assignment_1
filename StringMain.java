a54bd3495f7a3a57d1b9661d57680bb2d2a0aa5f
Perezonance <53836343+Perezonance@users.noreply.github.com>
2019-08-18 13:06:16 -0400

    Add files via upload

---
+++ StringMain.java
@@ -0,0 +1,11 @@
+package assignment_1;
+
+public class StringMain {
+	public static void main(String[] args) {
+		int x = 10;
+		
+		String s = String.valueOf(x);
+		
+		System.out.println(s);
+	}
+}
